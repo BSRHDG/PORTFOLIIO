d536eb6e2a7ae666436bcc2865f7c050bce41e8a
BSRHDG <70511494+BSRHDG@users.noreply.github.com>
2021-12-16 20:14:59 +0900

    2021-12-16
    2021-12-16

--- main.js
+++ main.js
@@ -1,117 +1,116 @@
-'use strict'
+"use strict";
 
 // Make navbar transparent when it is on the top
-const navbar = document.querySelector('#navbar');
+const navbar = document.querySelector("#navbar");
 const navbarHeight = navbar.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-    if (window.scrollY > navbarHeight) {
-        navbar.classList.add('navbar--dark');
-    } else {
-        navbar.classList.remove('navbar--dark');
-    }
-})
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
 
 // Navbar toggle button for small screen
-const NavbarToggleBtn = document.querySelector('.navbar__toggle-button');
-NavbarToggleBtn.addEventListener('click', () => {
-    navbarMenu.classList.toggle('open');
-})
+const NavbarToggleBtn = document.querySelector(".navbar__toggle-button");
+NavbarToggleBtn.addEventListener("click", () => {
+  navbarMenu.classList.toggle("open");
+});
 
 //functions
 function scrollIntoView(selector) {
-    const scrollTo = document.querySelector(selector);
-    scrollTo.scrollIntoView({ behavior: 'smooth' });
+  const scrollTo = document.querySelector(selector);
+  scrollTo.scrollIntoView({ behavior: "smooth" });
 }
 
 // Handle scrolling when tapping on the navbar menu
-const navbarMenu = document.querySelector('.navbar__menu');
-navbarMenu.addEventListener('click', (event) => {
-    const target = event.target;
-    const link = target.dataset.link;
-    if (link == null) {
-        return;
-    }
-    navbarMenu.classList.remove('open');
-    scrollIntoView(link);
-    
-})
+const navbarMenu = document.querySelector(".navbar__menu");
+navbarMenu.addEventListener("click", (event) => {
+  const target = event.target;
+  const link = target.dataset.link;
+  if (link == null) {
+    return;
+  }
+  navbarMenu.classList.remove("open");
+  scrollIntoView(link);
+});
 
 // Handle click on "contact me" button on home
-const ContactMeBtn = document.querySelector('.home__contact')
-ContactMeBtn.addEventListener('click', () => {
-    scrollIntoView('#contact');
-})
+const ContactMeBtn = document.querySelector(".home__contact");
+ContactMeBtn.addEventListener("click", () => {
+  scrollIntoView("#contact");
+});
 
 // Handle scrolling then fade-out home
-const fadeoutHome = document.querySelector('.home__container');
+const fadeoutHome = document.querySelector(".home__container");
 const homeHeight = home.getBoundingClientRect().height;
-document.addEventListener('scroll', () => {
-    fadeoutHome.style.opacity = 1 - window.scrollY / homeHeight;
+document.addEventListener("scroll", () => {
+  fadeoutHome.style.opacity = 1 - window.scrollY / homeHeight;
 });
 
 // Show arrow-up button when scrolling down
-const arrowUp = document.querySelector('.arrow-up');
-document.addEventListener('scroll', () => {
-    if (window.scrollY > homeHeight / 2) {
-        arrowUp.classList.add('visible');
-    } else {
-        arrowUp.classList.remove('visible');
-    }
+const arrowUp = document.querySelector(".arrow-up");
+document.addEventListener("scroll", () => {
+  if (window.scrollY > homeHeight / 2) {
+    arrowUp.classList.add("visible");
+  } else {
+    arrowUp.classList.remove("visible");
+  }
 });
 
 // Handle click on "arrow-up" button
-arrowUp.addEventListener('click', () => {
-    scrollIntoView('#home');
-})
+arrowUp.addEventListener("click", () => {
+  scrollIntoView("#home");
+});
 
 // 1. 모든 섹션 요소들을 가지고 온다
 // 2. IntersectionObserver를 이용해서 모든 섹션들을 관찰한다
 // 3. 보여지는 섹션에 해당하는 메뉴 아이템을 활성화 시킨다
 
-const sectionIds = ['#home', '#about', '#skills', '#work', '#contact'];
-const sections = sectionIds.map(id => document.querySelector(id));
-const navItems = sectionIds.map(id =>
-    document.querySelector(`[data-link="${id}"]`)
+const sectionIds = ["#home", "#about", "#skills", "#work", "#contact"];
+const sections = sectionIds.map((id) => document.querySelector(id));
+const navItems = sectionIds.map((id) =>
+  document.querySelector(`[data-link="${id}"]`)
 );
 
 let selectedNavIndex = 0;
 let selectedNavItem = navItems[0];
 function selectNavItem(selected) {
-    selectedNavItem.classList.remove('active');
-    selectedNavItem = selected;
-    selectedNavItem.classList.add('active');
+  selectedNavItem.classList.remove("active");
+  selectedNavItem = selected;
+  selectedNavItem.classList.add("active");
 }
 
 const observerOptions = {
-    root: null,
-    rootMargin: '0px',
-    threshold: 0.3,
+  root: null,
+  rootMargin: "0px",
+  threshold: 0.3,
 };
 
 const observerCallback = (entries, observer) => {
-    entries.forEach(entry => {
-        if (!entry.isIntersecting && entry.intersectionRatio > 0) {
-            const index = sectionIds.indexOf(`#${entry.target.id}`);
-            // 스크롤링이 아래로 되어서 페이지가 올라옴
-            if(entry.boundingClientRect.y < 0) {
-                selectedNavIndex = index + 1;
-            } else {
-                selectedNavIndex = index - 1;
-            }
-        }
-    });
+  entries.forEach((entry) => {
+    if (!entry.isIntersecting && entry.intersectionRatio > 0) {
+      const index = sectionIds.indexOf(`#${entry.target.id}`);
+      // 스크롤링이 아래로 되어서 페이지가 올라옴
+      if (entry.boundingClientRect.y < 0) {
+        selectedNavIndex = index + 1;
+      } else {
+        selectedNavIndex = index - 1;
+      }
+    }
+  });
 };
 const observer = new IntersectionObserver(observerCallback, observerOptions);
-sections.forEach(section => observer.observe(section));
+sections.forEach((section) => observer.observe(section));
 
-window.addEventListener('scroll', () => {
-    if (window.scrollY === 0) {
-        selectedNavIndex = 0;
-    } else if (
-        Math.round(window.scrollY + window.innerHeight) >=
-        document.body.clientHeight
-    ) {
-        selectedNavIndex = navItems.length - 1;
-    }
-        selectNavItem(navItems[selectedNavIndex]);
+window.addEventListener("scroll", () => {
+  if (window.scrollY === 0) {
+    selectedNavIndex = 0;
+  } else if (
+    Math.round(window.scrollY + window.innerHeight) >=
+    document.body.clientHeight
+  ) {
+    selectedNavIndex = navItems.length - 1;
+  }
+  selectNavItem(navItems[selectedNavIndex]);
 });
